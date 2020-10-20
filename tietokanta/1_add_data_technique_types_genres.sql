8af8e0cd9fb482ba502c8186fdf13f52aa948ca7
teijatestaaja <teija.alasalmi@gmail.com>
2020-10-20 14:20:44 +0300

    Korjattu typot

--- tietokanta/1_add_data_technique_types_genres.sql
+++ tietokanta/1_add_data_technique_types_genres.sql
@@ -1,6 +1,6 @@
 -- Tekniikan tyypit 1-7
 INSERT INTO TekniikanTyyppi (tekniikan_tyyppi_FI, tekniikan_tyyppi_JP) VALUES ('Perustekniikka', 'Kihon');                     -- 1
-INSERT INTO TekniikanTyyppi (tekniikan_tyyppi_FI, tekniikan_tyyppi_JP) VALUES ('Yhdistelmätekniikka', 'Renrakuwaza');          -- 2
+INSERT INTO TekniikanTyyppi (tekniikan_tyyppi_FI, tekniikan_tyyppi_JP) VALUES ('Yhdistelmätekniikka', 'Renraku waza');          -- 2
 INSERT INTO TekniikanTyyppi (tekniikan_tyyppi_FI, tekniikan_tyyppi_JP) VALUES ('Murskaus', 'Tameshiwari');                     -- 3
 INSERT INTO TekniikanTyyppi (tekniikan_tyyppi_FI, tekniikan_tyyppi_JP) VALUES ('Suosikki perustekniikka', 'Tokui kihon');      -- 4
 INSERT INTO TekniikanTyyppi (tekniikan_tyyppi_FI, tekniikan_tyyppi_JP) VALUES ('Essee', 'Essay');                              -- 5
@@ -19,4 +19,4 @@ INSERT INTO TekniikanLaji (tekniikan_laji_FI, tekniikan_laji_JP) VALUES ('Käsit
 INSERT INTO TekniikanLaji (tekniikan_laji_FI, tekniikan_laji_JP) VALUES ('Shorin-Ryu', 'Shorin-Ryu');                          -- 11
 INSERT INTO TekniikanLaji (tekniikan_laji_FI, tekniikan_laji_JP) VALUES ('Shorei-Ryu', 'Shorei-Ryu');                          -- 12
 INSERT INTO TekniikanLaji (tekniikan_laji_FI, tekniikan_laji_JP) VALUES ('Muu alkuperä', 'Muu alkuperä');                      -- 13
-INSERT INTO TekniikanLaji (tekniikan_laji_FI, tekniikan_laji_JP) VALUES ('Vapaa yhdistelmä', 'Renrakuwaza');                   -- 14
+INSERT INTO TekniikanLaji (tekniikan_laji_FI, tekniikan_laji_JP) VALUES ('Vapaa yhdistelmä', 'Renraku waza');                   -- 14
